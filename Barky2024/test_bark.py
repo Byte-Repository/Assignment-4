8894e2050bc4cdab857afcc3f5c2fc115f6ad721
Cesar Ortega <cortegafrias1@buffs.wtamu.edu>
2024-03-18 14:31:57 -0500

    Initial Commit

---
+++ Barky2024/test_bark.py
@@ -0,0 +1,23 @@
+# test_bark.py
+
+# Import necessary modules
+import pytest
+
+# Define test functions
+def test_clear_screen():
+    # Test clear_screen function
+    pass  # Add test logic here
+
+def test_Option():
+    # Test Option class
+    pass  # Add test logic here
+
+def test_print_options():
+    # Test print_options function
+    pass  # Add test logic here
+
+# Add more test functions to cover other functionalities in bark.py
+
+# Run the tests if the file is executed directly
+if __name__ == "__main__":
+    pytest.main()
